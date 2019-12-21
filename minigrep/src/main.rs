9ba9bceecf196d683d1cc7f767b17b2e948dc01f
Troy Giorshev <troygiorshev@gmail.com>
2019-12-20 20:03:35 -0500

    Make Config struct, fix docs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,25 +1,39 @@
-/// Troy Giorshev  
-/// Started: 2019-11-01  
-/// Finished:  
-/// From <https://doc.rust-lang.org/book/ch12-00-an-io-project.html>  
-/// I haven't gone through Ch 14 yet, so my documentation will be ~~a little~~ rough.
-/// TODO: Find a (the?) Rust style guide.  PEP 8 equivalent?
+//! Troy Giorshev  
+//! Started: 2019-11-01  
+//! Finished:  
+//! From <https://doc.rust-lang.org/book/ch12-00-an-io-project.html>  
+//! I haven't gone through Ch 14 yet, so my documentation will be ~~a little~~ rough.  
+//! TODO: Find a (the?) Rust style guide.  PEP 8 equivalent?
 
 use std::env;
 use std::fs;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();  // Iterators as usual
+    let args: Vec<String> = env::args().collect(); // Iterators as usual
     // println!("{:?}", args); // As usual, [0] is the path to the binary itself
 
-    let pattern = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args); // ok
 
-    println!("Search Pattern: {}", pattern);
-    println!("In file: {}", filename);
+    println!("Search Pattern: {}", config.pattern);
+    println!("In file: {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
-        .expect(&format!("Reading file `{}` failed", filename));
+    let contents = fs::read_to_string(&config.filename)
+        .expect(&format!("Reading file `{}` failed", config.filename));
 
     println!("\nText:\n\n{}", contents);
+}
+
+struct Config {
+    pattern: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // I'll learn a better way than clone() later
+        let pattern = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { pattern, filename }
+    }
 }
